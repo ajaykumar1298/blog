8c3c8a886161ebf31c054a2e74ccddfb287aed7b
ajaykumar1298 <ajaykumarak1209@gmail.com>
2025-03-26 23:24:04 +0530

    done some changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,34 @@ import "./App.css";
 import { useDispatch } from "react-redux";
 import auth from "./appwrite/auth";
 import { login, logout } from "./store/authSlice";
+import { Footer, Header } from "./components";
 
 function App() {
   let [loader, setLoader] = useState(true);
   let dispatch = useDispatch();
 
   useEffect(() => {
-    auth.getCurrentUser().then((userData) => {
-      if (userData) {
-        dispatch(login(userData));
-      } else {
-        dispatch(logout());
-      }
-    });
+    auth
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login(userData));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoader(false));
   }, []);
 
-  return <>hello world</>;
+  if (loader) return null;
+
+  return (
+    <>
+      <Header />
+      hey
+      <Footer />
+    </>
+  );
 }
 
 export default App;
