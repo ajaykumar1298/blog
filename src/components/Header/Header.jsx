16110480340201fa75c3b069dfbca4cff585cf20
ajaykumar1298 <ajaykumarak1209@gmail.com>
2025-03-28 03:14:56 +0530

    forward ref

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { LogoutBtn, Container, Logo } from "../index";
-import { Links } from "react-router-dom";
+import { Link, Links } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
@@ -35,7 +35,38 @@ function Header() {
       active: authStatus,
     },
   ];
-  return <div>Header</div>;
+  return (
+    <header className="py-3 shadow bg-grey-500">
+      <Container>
+        <nav className="flex">
+          <div className="m-4">
+            <Link to="/">
+              <Logo width="70px" />
+            </Link>
+          </div>
+          <ul className="flex ml-auto">
+            {navitem.map((item) =>
+              item.active ? (
+                <li key={item.name}>
+                  <button
+                    className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
+                    onClick={navigate(item.slug)}
+                  >
+                    {item.name}
+                  </button>
+                </li>
+              ) : null
+            )}
+            {authStatus && (
+              <li>
+                <LogoutBtn />
+              </li>
+            )}
+          </ul>
+        </nav>
+      </Container>
+    </header>
+  );
 }
 
 export default Header;
